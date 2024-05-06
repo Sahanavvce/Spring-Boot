47a16263e8ca89f2413bb64998d6e74067f67d34
Sahana C <86734154+Sahanavvce@users.noreply.github.com>
2024-05-06 10:47:32 +0530

    Updated

--- demo.DB/src/test/java/com/example/REST/demo/DB/ApplicationTests.java
+++ demo.DB/src/test/java/com/example/REST/demo/DB/ApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles(value = {("uat")})
+@ActiveProfiles(value = {("dev","prod","uat")})
 class ApplicationTests {
 
 	@Test
